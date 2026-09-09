f0df590eeeb36c9e5802a89ebe3bd162fd1b622e
Ahmet Kılıç <41ahmetkilic@gmail.com>
2026-09-09 12:04:24 +0300

    Publish Branchline v0.3.1

--- Casks/branchline.rb
+++ Casks/branchline.rb
@@ -1,8 +1,8 @@
 cask "branchline" do
-  version "0.3.0,7"
-  sha256 "a789d5b29f21d70e1cb944a65824b13fa8127146d4f4910b3356e79342c60d92"
+  version "0.3.1,8"
+  sha256 "996b4a43bc79502f0b0b8dc3cf13c7695f61382a0f6a9434193f2499966a5dc7"
 
-  url "https://github.com/ahmet12/branchline-releases/releases/download/v0.3.0/Branchline-0.3.0-7.dmg"
+  url "https://github.com/ahmet12/branchline-releases/releases/download/v0.3.1/Branchline-0.3.1-8.dmg"
   name "Branchline"
   desc "Native Git client for macOS by Kylindravia"
   homepage "https://github.com/ahmet12/branchline-releases"
